9faa3be0c575ccf3ff7b6fb67be37dbc51006e38
IcaroD <icaro.davila.santos@gmail.com>
2024-01-25 17:25:25 -0300

    initial commit

---
+++ Pedidos/Program.cs
@@ -0,0 +1,51 @@
+﻿using Pedidos.Entities;
+using Pedidos.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Pedidos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, date);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            string status = Console.ReadLine();
+            OrderStatus os = Enum.Parse<OrderStatus>(status);
+
+            Order order = new Order(DateTime.Now, os, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product name: ");
+                name = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(name, price);
+                OrderItem item = new OrderItem(quantity, price, product);
+
+                order.Items.Add(item);
+            }
+
+            Console.WriteLine(order);
+        }
+    }
+}
